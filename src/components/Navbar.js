08748f7b1bf073f40845b7745a4666aad2221c24
sowmik-sec <sowmik.sec@gmail.com>
2023-02-18 21:30:59 +0600

    nested route explored

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,12 +9,15 @@ const Navbar = () => {
     };
   };
   return (
-    <nav>
+    <nav className="primary-nav">
       <NavLink style={navLinkStyles} to="/">
         Home
       </NavLink>
       <NavLink style={navLinkStyles} to="/about">
         About
+      </NavLink>
+      <NavLink style={navLinkStyles} to="/products">
+        Products
       </NavLink>
     </nav>
   );
